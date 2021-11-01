147de43fdbc206c785882a4b103ac95b626a8b86
Daniel Krol <orblivion@gmail.com>
2021-10-31 20:40:19 -0400

    Install plugins from a local dir with patches.

--- .sandstorm/build.sh
+++ .sandstorm/build.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 cd /opt/app
 
-./scripts/checkout-etherpad
+./scripts/checkout-repos
 ./scripts/apply-patches
 
 export NODE_ENV=production
@@ -20,30 +20,16 @@ cd etherpad-lite
 npm install @mapbox/node-pre-gyp
 npm install sqlite3
 
-# Install plugins
-npm install $(cat ../plugins)
+# Install basic plugins (from npm)
+npm install $(cat ../basic-plugins)
+
+../scripts/install-edited-plugins
 
 # Etherpad tries to touch $plugin/.ep_initialized on first run,
 # so let's symlink that to somewhere writable:
-for plugin in $(cat ../plugins); do
+for plugin in $(cat ../basic-plugins; echo ep_author_neat; echo ep_comments_page); do
 	ln -sf /var/plugins-initialized/$plugin node_modules/$plugin/.ep_initialized
 done
 
-
-# Install capnp
-#
-# capnp is used for posting Sandstorm "activities"
-#
-# As of Oct 2021: The npm package is out of date. The node14 branch of the git
-# repo is used on Sandstorm itself now. `npm install kenton/node-capnp#<ref>`
-# does not work as of now, so we install the way we do.
-#
-# `npm install <something>` seems to delete anything it doesn't
-# recognize. So we have to do our hack installations after all our proper npm
-# installations.
-
-# current tip of node14 branch. Using hash for reproducibility.
-CAPNP_HASH=ca17e686f267e1fcce20a2ed9583847b4528cd8f
-
-(cd node_modules && git clone https://github.com/kentonv/node-capnp.git capnp)
-(cd node_modules/capnp && git checkout $CAPNP_HASH && npm install)
+cd ..
+./scripts/install-capnp
--- scripts/config.sh
+++ scripts/config.sh
@@ -1,2 +1,3 @@
-
 etherpad_base_revision='1.8.14'
+ep_author_neat_base_revision='0.0.31'
+ep_comments_page_base_revision='df08734700e08fed99ef06ca5a17d16c86c73822' # tag v0.1.78 per github
