e0fdd49ea4b5e3053517878f570c9d141bdff89a
Daniel Krol <orblivion@gmail.com>
2022-01-20 20:10:49 -0500

    Refer to checkouts by hash
    (a bit more secure and repeatable)

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,3 +1,3 @@
-etherpad_base_revision='1.8.14'
-ep_author_neat_base_revision='0.0.31'
-ep_comments_page_base_revision='df08734700e08fed99ef06ca5a17d16c86c73822' # tag v0.1.78 per github
+etherpad_base_revision='571b37b772c324d6f5eddcd3aecf3e02ffcac1c3' # tag '1.8.14' per github
+ep_author_neat_base_revision='27332b3e5936173bea445149a342cb2e04400dee' # tag '0.0.31' per github
+ep_comments_page_base_revision='df08734700e08fed99ef06ca5a17d16c86c73822' # tag 'v0.1.78' per github
